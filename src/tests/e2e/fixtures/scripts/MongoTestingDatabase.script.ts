6e5d6a2cb24a5107f36965e2d63493caed187f76
Miqueias Fernandes <miqueias.fernandes@bitfy.app>
2023-07-01 14:21:14 -0300

    feat: add "singleton" verification

--- src/tests/e2e/fixtures/scripts/MongoTestingDatabase.script.ts
+++ src/tests/e2e/fixtures/scripts/MongoTestingDatabase.script.ts
@@ -2,17 +2,26 @@ import { CommanderUtil } from "./Commander.uti";
 import { ITestingDatabasePort } from "./TestingDatabase.port";
 
 export class MongoTestingDatabaseAdapter implements ITestingDatabasePort {
+  static isInited = false;
+
   initDatabase(): Promise<void> {
     const username = process.env.DATABASE_USER;
     const password = process.env.DATABASE_PASSWORD;
     const database = process.env.DATABASE_NAME;
 
-    return CommanderUtil.execute(
-      `docker run --name test-mongo -e MONGO_INITDB_DATABASE=${database} MONGO_INITDB_ROOT_USERNAME=${username} MONGO_INITDB_ROOT_PASSWORD=${password} -d mongo`
-    );
+    if (!MongoTestingDatabaseAdapter.isInited) {
+      CommanderUtil.execute(
+        `docker run --name test-mongo -e MONGO_INITDB_DATABASE=${database} MONGO_INITDB_ROOT_USERNAME=${username} MONGO_INITDB_ROOT_PASSWORD=${password} -d mongo`
+      );
+
+      MongoTestingDatabaseAdapter.isInited = true;
+      return;
+    }
   }
 
   closeDatabase(): Promise<void> {
-    return CommanderUtil.execute(`docker stop $(docker ps -q)`);
+    if (!MongoTestingDatabaseAdapter.isInited) {
+      return CommanderUtil.execute(`docker stop $(docker ps -q)`);
+    }
   }
 }
