fd79fc09d59e768424c56adc3496452d8dd8bc0c
Miqueias Fernandes <miqueias.fernandes@bitfy.app>
2023-07-01 14:16:23 -0300

    test: add e2e synchronizationt test proptotype

---
+++ src/tests/e2e/SynchronizationController.e2e-spec.ts
@@ -0,0 +1,31 @@
+import request from "supertest";
+import { BootstrapModule } from "@bootstrap/Bootstrap.module";
+import { INestApplication } from "@nestjs/common";
+import { Test } from "@nestjs/testing";
+import { ITestingDatabasePort } from "./fixtures/scripts/TestingDatabase.port";
+import { TestingDatabaseFactory } from "./fixtures/scripts/TestingDatabaseFactory";
+
+describe("Synchronization", () => {
+  let app: INestApplication;
+  let database: ITestingDatabasePort;
+
+  beforeAll(async () => {
+    database = TestingDatabaseFactory.makeTestingDatabase();
+
+    database.initDatabase();
+
+    const testingModule = await Test.createTestingModule({
+      imports: [BootstrapModule],
+    }).compile();
+
+    app = testingModule.createNestApplication();
+    await app.init();
+  });
+  afterAll(async () => {
+    database.closeDatabase();
+    await app.close();
+  });
+  test("[POST] /synchronize", async () => {
+    return request(app.getHttpServer()).post("/synchronize").expect(201);
+  });
+});
