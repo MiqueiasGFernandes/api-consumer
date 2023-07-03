ebc08a9e769e6207b0040feb75b6768ad3b6b186
Miqueias Fernandes <miqueias.fernandes@bitfy.app>
2023-07-03 19:15:02 -0300

    style: add use-cases constant

--- report-worker/src/domain/use-cases/GenerateCsv.use-case.ts
+++ report-worker/src/domain/use-cases/GenerateCsv.use-case.ts
@@ -3,3 +3,5 @@ import { InternalUserModel } from '@domain/models';
 export interface IGenerateCsvUseCase {
   generate(data: InternalUserModel[]): Promise<Buffer>;
 }
+
+export const GENERATE_CSV_USE_CASE = 'GENERATE_CSV_USE_CASE';
--- report-worker/src/domain/use-cases/SendCsv.use-case.ts
+++ report-worker/src/domain/use-cases/SendCsv.use-case.ts
@@ -1,3 +1,5 @@
 export interface ISendCsvUseCase {
   send(csv: Buffer): Promise<string>;
 }
+
+export const SEND_CSV_USE_CASE = 'SEND_CSV_USE_CASE';
